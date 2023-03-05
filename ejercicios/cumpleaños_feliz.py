a79d610add20401122703aff4a094e0bcc083871
Neobraingit <Maervca2@icloud.com>
2023-03-05 14:34:38 +0100

    print

---
+++ ejercicios/cumpleaños_feliz.py
@@ -0,0 +1,7 @@
+# Vamos a combinar concatenación y repetición de strings para reproducir la canción " Cumpleaños feliz"
+
+cumpleaños = 'Cumpleaños feliz\n'
+print (cumpleaños * 2)
+deseo = 'Te deseamos todos\n'
+print (deseo)
+print (cumpleaños)
---
+++ print.py
@@ -0,0 +1,16 @@
+### LA FUNCIÓN PRINT ###
+
+# La función print nos sirve para mostrar datos por pantalla
+
+print ('Hello world') # No muestra el string al printear 
+
+s1 = 'Esto es una variable ' # Printeamos una variable
+print (s1)
+
+s2 = str(1234355)
+print (s2) # Printeamos un número entero
+
+print (s1 + s2) # No podemos concatenar un string con un entero, por lo que debemos hacer el casteo del dato (podemos sustituir el + por comas)
+
+
+
