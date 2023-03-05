2a99d2f7bdf4d787446cc788abd51ed67c85ea81
Neobraingit <Maervca2@icloud.com>
2023-03-05 20:32:34 +0100

    operadores_decisión

---
+++ operadores_decision.py
@@ -0,0 +1,16 @@
+### OPERADORES DE DECISIÓN ###
+
+# if
+# Si queremos comprobar si se cumple alguna condición  usaremos if
+
+if True:
+    print ('Se cumple la condición')
+    
+# else
+# Si no se cumple la conmdición se ejecutará lo que hay en el else
+
+nombre = 'Marcos'
+if nombre != 'Marcos':
+    print ('Ese no es su nombre')
+else:
+    print ('Nombre correcto')
