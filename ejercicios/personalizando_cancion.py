140b5c4f192607eb1fd4f3ab36cb9e705792845a
Neobraingit <Maervca2@icloud.com>
2023-03-05 15:47:09 +0100

    ejercicios

---
+++ ejercicios/personalizando_cancion.py
@@ -0,0 +1,6 @@
+nombre = input('Indica el destinatario de la canción: ')
+cumpleaños = 'Cumpleaños feliz\n'
+print (cumpleaños * 2)
+deseo = 'Te deseamos todos\n'
+print (deseo.replace('todos', nombre))
+print (cumpleaños)
