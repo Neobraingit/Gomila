db326df4e3939bfc0d14813b8cbd7a4b74eb8cd5
Neobraingit <Maervca2@icloud.com>
2023-03-05 20:58:01 +0100

    ejercicios

---
+++ ejercicios/espacios_blanco.py
@@ -0,0 +1,10 @@
+# Dado un string, vamos a comprobar si contiene espacios en blancoy, en caso de ser cierto, contar cuántos tiene
+
+string = 'El nombre de mis gatas son Pelusa y Dune'
+
+if ' ' in string:
+    print ('El string tiene {} espacios en blanco'.format(string.count(' ')))
+    
+    
+    
+    
