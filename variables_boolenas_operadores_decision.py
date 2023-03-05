2872913e0c66a62da15930b26e793d327b64991c
Neobraingit <Maervca2@icloud.com>
2023-03-05 19:01:10 +0100

    booleanos_decision

---
+++ variables_boolenas_operadores_decision.py
@@ -0,0 +1,6 @@
+### VARIABLES BOOLEANAS Y OPERADORES DE DECISIÓN ###
+
+# Los datos booleanos solo pueden tener 2 valores:  True o False
+es_adulto = True
+print (type(es_adulto)) # Con esto sabemos que tipo de valor es; en este caso es un dato booleano que nos da True
+
