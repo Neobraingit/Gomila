0e337e0e41077ffd9fd23cbc7e6d02739e3a9afe
Neobraingit <Maervca2@icloud.com>
2023-03-05 12:18:46 +0100

    cadenas

---
+++ cadenas.py
@@ -0,0 +1,15 @@
+# VARIABLES DE TIPO STRING
+
+string1 = 'Esto es un a variable de tipo string'
+print (string1)
+
+# STRING LITERAL
+
+'Esto es un string por que va entre comillas'
+
+# \\ Símbolo escapante
+# \' Comilla simple
+# \" Comillas dobles
+# \n Salto línea
+# \t Tbulación horizontal
+
