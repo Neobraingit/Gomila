eaf11ed512604c8b3c97d6c6f8140e1400622959
Neobraingit <Maervca2@icloud.com>
2023-03-05 14:52:12 +0100

    substrings

--- substrings.py
+++ substrings.py
@@ -7,4 +7,5 @@ print (s[0]) # Accedemos al primer caracter de la frase ya que empiezan a contar
 print (s[0:4]) # Nos irá hasta el tres ya que el último índice nunca lo incluye
 print (s[-1]) # Empezamos a contar por el final
 print (s[::-1]) # Le damos la vuelta al string
+print (s[:7]) # Si no podemos nada antes de los dos puntos, se considera cero
 
