2f5ab20b84d50b12e7af8f7f2b973288e09442a7
Neobraingit <Maervca2@icloud.com>
2023-03-05 19:37:40 +0100

    operadores_comparación

---
+++ operadores_comparacion.py
@@ -0,0 +1,16 @@
+### OPERADORES COMPARACIÓN ###
+
+# > mayor que
+# < menor que
+# == igual que
+# >= mayor o igual que
+# <= menor o igual que
+# != diferente que
+
+# Las siguientes operaciones nos daran como resultado un valor booleano
+
+print (7 == 7.0)
+print (3.14 > 9)  # En estas 3 operaciones usamos los operadores de comparación
+print (7 != '7')
+
+
