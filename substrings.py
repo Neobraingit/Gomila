0907a7c77c610955ba900460d4dbb4909f08dd10
Neobraingit <Maervca2@icloud.com>
2023-03-05 14:53:50 +0100

    substrings

--- substrings.py
+++ substrings.py
@@ -8,4 +8,5 @@ print (s[0:4]) # Nos irá hasta el tres ya que el último índice nunca lo inclu
 print (s[-1]) # Empezamos a contar por el final
 print (s[::-1]) # Le damos la vuelta al string
 print (s[:7]) # Si no podemos nada antes de los dos puntos, se considera cero
+print (s[0:]) # Si no ponemos nada a la derecha de los dos puntos se considera que va hasta el final
 
