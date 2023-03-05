9339dd491c5839e50aefdb0a63bb1c4aa52d4a2d
Neobraingit <Maervca2@icloud.com>
2023-03-05 14:48:27 +0100

    substrings

---
+++ substrings.py
@@ -0,0 +1,10 @@
+### SUBSTRINGS ###
+
+# Se puede acceder a un substrings con slicing
+
+s = 'Soy fan del Heavy Metal'
+print (s[0]) # Accedemos al primer caracter de la frase ya que empiezan a contar desde cero
+print (s[0:4]) # Nos irá hasta el tres ya que el último índice nunca lo incluye
+print (s[-1]) # Empezamos a contar por el final
+print (s[::-1]) # Le damos la vuelta al string
+
