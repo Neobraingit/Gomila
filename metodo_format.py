d11f0a9c556680156a0971e6e7e2e56433cd684d
Neobraingit <Maervca2@icloud.com>
2023-03-05 14:43:34 +0100

    format

---
+++ metodo_format.py
@@ -0,0 +1,8 @@
+### MÉTODO FORMAT ###
+
+# Para concatenar variable dentro de un print usaremos el método format()
+nombre = 'Marcos'
+apellido = 'Carmona'
+edad = 48
+
+print ('Mi nombre es {} y mi apellido es {} y tengo {} años'.format(nombre, apellido, edad))
