d85994733af9ffe60a148b56c6e5611009aa3f12
Neobraingit <Maervca2@icloud.com>
2023-03-05 21:21:39 +0100

    ejercicios

--- ejercicios/vocal_mas_comun.py
+++ ejercicios/vocal_mas_comun.py
@@ -2,4 +2,10 @@ frase = 'Esto es una prueba de ejercicio...'
 frase = frase.lower()
 letra = input('Introduce la letra a contar: ')
 if letra in frase:
-    print ('La frase tiene {} letras'.format(frase.count(letra)))
+    print ('La frase tiene {} '.format(frase.count(letra)))
+    
+
+# Letra más común
+
+
+    
