996bdd530dc89a5e71d875d4bd9af5a8c3252653
Neobraingit <Maervca2@icloud.com>
2023-03-05 14:08:49 +0100

    cadenas

--- cadenas.py
+++ cadenas.py
@@ -13,3 +13,11 @@ print (string1)
 # \n Salto línea
 # \t Tbulación horizontal
 
+s1 = "Juan: \"me gusta el chocolate\"" # Aquí con la barra escapante  podemos poner dos veces las comillas dobles
+print (s1)
+
+s2 = 'Ricardo dijo: \'me gusta la playa\'' # Lo mismo que lo anterior pero con comillas simples
+print (s2)
+
+s3 = 'Con diez cañones por banda, \nviento en popa a toda vela' # Aquí hacemos un salto de línea
+print (s3)
