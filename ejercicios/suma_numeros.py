440eb1dec5858fd5c5663a9c0b144a1632484129
Neobraingit <Maervca2@icloud.com>
2023-03-06 19:23:14 +0100

    ejercicios

---
+++ ejercicios/suma_numeros.py
@@ -0,0 +1,8 @@
+num = float(input('Introduce un número: '))
+suma = 0
+while num != 0:
+    suma += num
+    num = float(input('Introduce un número: '))
+print ('La suma es {}'.format(suma))
+    
+    
