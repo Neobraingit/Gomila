389fdb2deabfb7f0d1024006be67edac28cd7484
Neobraingit <Maervca2@icloud.com>
2023-03-06 17:34:47 +0100

    ejercicios

--- ejercicios/par_impar.py
+++ ejercicios/par_impar.py
@@ -1,4 +1,4 @@
 # Vamos a comprobar si un número es par o impar con el operador ternario
 
 numero = int(input('Introduce un número: '))
-print ('es par') if numero % 2 == 0 else print ('es impar')
+print ('El número es par') if numero % 2 == 0 else print ('El número es impar')
