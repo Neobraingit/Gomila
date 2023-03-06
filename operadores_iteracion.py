f654ab2a1090042cb4a3a6ee4a01639355ad1cd8
Neobraingit <Maervca2@icloud.com>
2023-03-06 18:09:09 +0100

    operadores_iteracion

---
+++ operadores_iteracion.py
@@ -0,0 +1,19 @@
+### OPERADORES ITERACIÓN ###
+
+# while
+
+numero = 101
+while numero > 0:
+    numero -= 1
+    print ('El número es {}'.format(numero))
+    
+    
+numero = 1
+while numero <= 10:
+    print (numero)
+    numero += 1
+    
+# Comando break
+
+
+    
