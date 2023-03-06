6d5e3fe4e9f4bad658a57dff6cac65937ecb7105
Neobraingit <Maervca2@icloud.com>
2023-03-06 18:49:08 +0100

    operadores_iteracion

--- operadores_iteracion.py
+++ operadores_iteracion.py
@@ -13,7 +13,14 @@ while numero <= 10:
     print (numero)
     numero += 1
     
-# Comando break
+# Comando break: cortamos la ejecución
 
+# Combinación while else
 
+i = 10
+while i >= 0:
+    i -= 1
+    print (i)
+else: 
+    print ('La cuenta atrás ha finalizado¡¡')
     
