3ba95395141878705c1263ea0f6e54b30d82e1ca
Neobraingit <Maervca2@icloud.com>
2023-03-06 17:29:24 +0100

    operador_ternario

---
+++ operador_ternario.py
@@ -0,0 +1,6 @@
+### OPERADOR TERNARIO ###
+
+edad = 20 
+mayor = 'Eres mayor de edad'
+menor = 'Eres menor'
+print (mayor) if edad >= 18 else print (menor) # Así todo en una línea es un operador ternario
