09b6db0a2be63ba8eb80afdbae36b641629f8086
Neobraingit <Maervca2@icloud.com>
2023-03-07 20:52:49 +0100

    diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,6 @@
+### DICCIONARIOS ###
+
+# Se acceden a los objetos por clave y no por índice. Necesitan clave : valor.Admiten todo tipo de dato y se pueden modificar
+
+dic1 = {'Nombre' : 'Marcos', 'Apellido' : 'Carmona', 'Edad' : 48}
+
