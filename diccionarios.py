fc98af88a38e2358996e9860a1a46760a8744fe1
Neobraingit <Maervca2@icloud.com>
2023-03-08 18:20:28 +0100

    diccionarios

--- diccionarios.py
+++ diccionarios.py
@@ -41,5 +41,27 @@ print (diccionario['Nombre'])
 
 # Más métodos de diccionarios
 
+nombres = {'Nombre' : 'Marcos', 'Apellidos' : 'Carmona García', }
+
+print (nombres.clear()) # Borra todo el diccionario
+
+nombres2 = {'Nombre' : 'Marcos', 'Apellidos' : 'Carmona García', }
+
+print (nombres2.copy()) # Devuelve una copia del diccionario original
+
+dic3 = nombres2.fromkeys([1,2,3,4], [5,6,7,8]) # Devuelve como clave un iterable que le pasemos
+print (dic3)
+
+print (nombres2.get('Nombre')) # Devuelve el valor de una clave que le pasemos
+
+print (nombres2.pop('Nombre')) # Recibe como parámetro una clave, la elimina y devuelve su valor
+
+print (nombres2.setdefault('z', 6)) # Añadimos una clave valor o también funciona como el método .get()
+
+print (nombres2.update(dic4 = {'Nombre' : 'David'})) # Actualiza el diccionario con otro diccionario que le pasemos
+
+# Convertir a diccionario con dict()
+
+
 
 
