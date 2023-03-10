cc686aaff8a9fc99d87717302442e9ce7c3ea93d
Neobraingit <Maervca2@icloud.com>
2023-03-10 10:25:02 +0100

    conjuntos

--- conjuntos.py
+++ conjuntos.py
@@ -1,6 +1,7 @@
 ### CONJUNTOS ###
 
 # Son estructuras sin orden, son definidos con la función set() o bien pueden ir entre llaves y separados por comas
+# No se admiten elementos repetidos
 # Los conjuntos pueden tener diferentes tipos de datos pero estos no pueden ser modificados
 
 # Pueden construirse con la función set()
@@ -10,4 +11,6 @@ print (set1)
 
 # También pueden entre llaves
 
-set2 
+set2 = {1,2,3,4,5}
+
+print (set2)
