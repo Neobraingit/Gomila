35829a619e82c40ba2590d68f8bf87c413d6ba6f
Neobraingit <Maervca2@icloud.com>
2023-03-10 10:46:08 +0100

    tulplas

---
+++ ejercicios/numeros_enteros_tupla.py
@@ -0,0 +1,8 @@
+''' 
+Vamos a pedirle al usuario el número de números enteros que va a introducir por teclado. Para cada uno de esos números,
+vamos a crear una tupla y después guardaremos todas las tuplas en una lista que mostraremos por pantalla
+'''
+
+numero_entero = int(input('Dime la cantidad de númerros que vas a insertar: '))
+tupla = input()
+print (list(tupla))
--- tuplas.py
+++ tuplas.py
@@ -2,6 +2,16 @@
 
 # Pueden contener distintos tipos de datos
 # No pueden mutar
-# Van entre paréntesis
+# Van entre paréntesis aunque se pueden declarar sin ellos
+# Para convertir algo a tupla usamos la función tuple()
 
 tupla = (1,2,3,4,'Marcos')
+print (tupla)
+
+# Elementos de una tupla
+
+# Accedemos a los elementos usando slicing
+
+
+
+
