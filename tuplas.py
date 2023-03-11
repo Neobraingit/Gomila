385618cc1fe8738488d90ff9c0f294686afb7ba4
Neobraingit <Maervca2@icloud.com>
2023-03-11 08:53:54 +0100

    tuplas

--- tuplas.py
+++ tuplas.py
@@ -12,6 +12,12 @@ print (tupla)
 
 # Accedemos a los elementos usando slicing
 
+coches  = ('Renault', 'Toyota', 'Opel')
+print (coches[0])
+print (coches[0:3]) # El índice a la derecha de los dos puntos nunca será incluido
 
-
+if 'Toyota' in coches:
+    print (True)
+    
+# Unpacking
 
