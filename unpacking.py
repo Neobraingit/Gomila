b057665f44cd684a27cc5903e1353e5a5556114a
Neobraingit <Maervca2@icloud.com>
2023-03-13 17:04:59 +0100

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,11 @@
+### UNPACKING ###
+
+# Podemos extraer los elementos de una tupla en variables
+
+tupla = ('Marcos', 'Eva', 'Marta', 'David')
+(nombre1, nombre2, nombre3, nombre4)  = tupla
+
+print (nombre1)
+print (nombre2)
+print (nombre3)
+print (nombre4)
